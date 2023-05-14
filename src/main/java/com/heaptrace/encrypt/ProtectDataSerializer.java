a20fb72fbf447f3e86d047ad6a4fb16f6b0a0cf3
NareshSachin <misalsachin301@gmail.com>
2023-05-14 10:41:24 +0530

    Implmented PII data masking

---
+++ src/main/java/com/heaptrace/encrypt/ProtectDataSerializer.java
@@ -0,0 +1,17 @@
+package com.heaptrace.encrypt;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+public class ProtectDataSerializer extends JsonSerializer {
+
+    @Override
+    public void serialize(Object value, JsonGenerator gen, SerializerProvider serializerProvider) throws IOException {
+        String pii = value.toString().replaceAll("\\w(?=\\w{4})", "x");
+        gen.writeString(pii);
+    }
+
+}
---
+++ src/main/java/com/heaptrace/entity/AccountDetails.java
@@ -0,0 +1,18 @@
+package com.heaptrace.entity;
+import com.heaptrace.encrypt.MaskData;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AccountDetails {
+
+    private String accountHolderName;
+    @MaskData
+    private String accountNumber;
+    private String accountType;
+}
